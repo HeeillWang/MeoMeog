5d66f4ea5650dc1b657868303755863e936ad6f7
imola <pz6tiger@me.com>
2017-12-03 22:07:35 +0900

    add normalize and fix weight.csv file

--- Recommend_System/RuleBased.py
+++ Recommend_System/RuleBased.py
@@ -5,18 +5,25 @@ import  numpy as np
 class preference:
     #same order as in survey
     def __init__(self, korean, chinese, meat, soup, japanese, fastfood, flour, chicken, pizza, noodle, western, sashimi):
-        self.korean = korean
-        self.chinese = chinese
-        self.meat = meat
-        self.soup = soup
-        self.japanese = japanese
-        self.fastfood = fastfood
-        self.flour = flour
-        self.chicken = chicken
-        self.pizza = pizza
-        self.noodle = noodle
-        self.western = western
-        self.sashimi = sashimi
+        self.korean = self.normalizer(korean)
+        self.chinese = self.normalizer(chinese)
+        self.meat = self.normalizer(meat)
+        self.soup = self.normalizer(soup)
+        self.japanese = self.normalizer(japanese)
+        self.fastfood = self.normalizer(fastfood)
+        self.flour = self.normalizer(flour)
+        self.chicken = self.normalizer(chicken)
+        self.pizza = self.normalizer(pizza)
+        self.noodle = self.normalizer(noodle)
+        self.western = self.normalizer(western)
+        self.sashimi = self.normalizer(sashimi)
+
+    def normalizer(pref):
+        if pref >= 3:
+            pref = pref + 5
+
+        pref = pref / 10
+        return pref
 
 
 class userInput:
@@ -25,8 +32,11 @@ class userInput:
         self.gender = gender
         self.age = age
         self.preference = preference
-        self.pref_for_new_rest = pref_for_new_rest
-        self.pref_for_distance = pref_for_distance
+        self.pref_for_new_rest = self.normalizer(pref_for_new_rest)
+        self.pref_for_distance = self.normalizer(pref_for_distance)
+
+    def normalizer(pref):
+        return pref / 5
 
 
 class curInput:
@@ -37,22 +47,30 @@ class curInput:
         self.latitude = latitude
         self.longitude = longitude
 
+    def getPosition(self):
+        return (self.latitude, self.longitude)
+
 
 class restInfo:
     # same order as in restaurant information
-    def __init__(self, name, category, latitude, longitude, rate, startTime, endTime):
+    def __init__(self, name, category, latitude, longitude, globalRate, userRate, startTime, endTime):
         self.name = name
         self.category = category
         self.latitude = latitude
         self.longitude = longitude
-        self.rate = rate
+        self.globalRate = globalRate
+        self.userRate = userRate
         self.startTime = startTime
         self.endTime = endTime
+        self.score = 0
 
     # it calculate square of distance
     # notice that it does not do square root operation
-    def setDistance(self, cur_latitude, cur_longitude):
-        self.distance = (cur_latitude - self.latitude) * (cur_latitude - self.latitude) + (cur_longitude - self.longitude) * (cur_longitude - self.longitude)
+    def setDistance(self, position, norm):
+        if norm == 0:
+            self.distance = (position[0] - self.latitude) * (position[0] - self.latitude) + (position[1] - self.longitude) * (position[1] - self.longitude)
+        else:
+            self.distance = norm
 
     # weights is list of weight about this restaurant.
     # for example : weight for distance, weight for preference of category... etc.
@@ -60,6 +78,17 @@ class restInfo:
     def setWeight(self, weights):
         self.weight = weights
 
+    def getCategory(self):
+        return self.category
+
+    def getDistance(self):
+        return self.distance
+
+    def addScore(self, addValue):
+        self.score = self.score + addValue
+
+    def getScore(self):
+        return self.score
 
 
 # parse data(from server) -> to our class(preference, userInput, curInput, restInfo)
@@ -67,22 +96,47 @@ class restInfo:
 # class parseToClass:
 
 
-
-# argument : list of 'restInfo' objects.
+# argument : list of 'restInfo' objects, current user data, current data
 # load weights from csv file and then save it to restInfo
 # it must ensure that order of restaurants are same.
+# call first this function
 # returns : list of 'restInfo' objects.
 def loadWeightAndSaveToRest(rest_arr):
     # skip first row
     data = np.loadtxt("weight.csv", delimiter=",", dtype=np.float32, skiprows=1)
 
-    # need to inspect this part : "how to define weights? on each restaurants or globally?"
-    #for i in range(1, data.size):
-    #    rest_arr[i].setWeight(data[i])
+    for rest in rest_arr:
+        rest.setWeight(data[rest.getCategory()])
 
     return rest_arr
 
 
 # need to write this method
-def getRecommRest(usrinfo, date, restinfo):
-    print('nothing yet')
+# call next this function. distance setting contain this function
+# return output vector
+def getRecommRest(preference, usrinfo, curinfo, rest_arr):
+    # set distance
+    for rest in rest_arr:
+        rest.setDistance(curinfo.getPosition())
+
+    # distance normalize
+    minDist = rest_arr[0].getDistance()
+    maxDist = rest_arr[0].getDistance()
+
+    for rest in rest_arr:
+        if minDist > rest.getDistance():
+            minDist = rest.getDistance()
+
+        if maxDist < rest.getDistance():
+            maxDist = rest.getDistance()
+
+    distDiff = maxDist - minDist
+
+    for rest in rest_arr:
+        rest.setDistance((0, 0), (rest.getDistance() - minDist) / distDiff)
+
+    # need more code!
+    return 0
+
+if __name__ == "__main__":
+    #test
