9907f597d38327a2979cc9bb8e9b7e75e8308ace
hsherlcok <sk1528@naver.com>
2017-12-06 01:23:47 +0900

    Server POST request added

--- Server/server.py
+++ Server/server.py
@@ -4,12 +4,64 @@ from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 import MySQLdb
 from urlparse import parse_qs
 import time
+import json
+import cgi
 # import RuleBased
 
 DEBUG = True
 ADDR = ""
 PORT = 9999
 class MyHandler(BaseHTTPRequestHandler):
+    def _set_header(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        
+    def do_POST(self):
+        self._set_header()
+        
+        form = cgi.FieldStorage(
+            fp = self.rfile,
+            header = self.headers,
+            environ = {'REQUEST_METHOD' :'POST'}
+        )
+        
+        jsondata = form.getvalue['data']
+        if DEBUG:
+            print jsondata
+        
+        decode_json = json.load(jsondata)
+        if DEBUG:
+            print decode_json
+        
+        # get current local time of server
+        now = time.localtime()
+        date = "%d:%d:%d" % (now.tm_hour, now.tm_min, now.tm_sec)
+        if DEBUG:
+            print date
+        
+        # get restaurant information from database
+        cur.execute("SELECT * FROM RestInfo")           # Send QUERY
+        
+        # Get result and put it into dictionary variable
+        restinfo = []
+        for row in cur.fetchall():
+            tmp = dict()
+            tmp['id'] = row[0]
+            tmp['name'] = row[1]
+            tmp['lat'] = row[2]
+            tmp['long'] = row[3]
+            tmp['rating'] = row[4]
+
+            restinfo.append(tmp)
+        if DEBUG:
+            print restinfo
+        
+        # usrinfo = data
+        # RuleBased.getRecommRest(usrinfo, date, restinfo)
+        
+        self.wfile.write("Rule Based Algorithm")
+        
     def do_GET(self):
         spath = self.path[1:]
         
@@ -17,59 +69,61 @@ class MyHandler(BaseHTTPRequestHandler):
         
         print pathinfo
         
-        if pathinfo == 'rulebased':
-            if DEBUG :
-                print ("Rule Based Algorithm request")
-                
-            # Do something
-            # Make proper output with json format and write it into 'wfile'
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            
-            # get GET responses & parse it 
-            data = parse_qs(self.path[1:].split('?')[1])
-            if DEBUG:
-                print data['data'][0]
-            
-            # get current local time of server
-            now = time.localtime()
-            date = "%d:%d:%d" % (now.tm_hour, now.tm_min, now.tm_sec)
-            if DEBUG:
-                print date
-            
-            # get restaurant information from database
-            cur.execute("SELECT * FROM RestInfo")           # Send QUERY
-            
-            # Get result and put it into dictionary variable
-            restinfo = []
-            for row in cur.fetchall():
-                tmp = dict()
-                tmp['id'] = row[0]
-                tmp['name'] = row[1]
-                tmp['lat'] = row[2]
-                tmp['long'] = row[3]
-                tmp['rating'] = row[4]
+        # Do something
+        # Make proper output with json format and write it into 'wfile'
+        self._set_header()
+        """
+        # get GET responses & parse it 
+        # {"soup":3,"chicken":3,"gender":"M","ch":3,"pref_dist":4
+        # "jp":3,"pref_new_rest":4,"kr":3,"noodle":3,"long":36.24
+        # "fast":3,"pizza":3,"flour":3,"meat":3,"sashimi":3,"western":3,"age":25,"lat":126.1234}
+        data = parse_qs(self.path[1:].split('?')[1])
+        gender = data['gender']
+        ch = data['ch']
+        soup = data['soup']
+        chicken = data['chicken']
+        pref_dist = data['pref_dist']
+        jp = data['jp']
+        pref_new_rest = data['pref_new__rest']
+        kr = data['kr']
+        noodle = data['noodle']
+        longitute = data['long']
+        fast = data['fast']
+        pizza = data['pizza']
+        flour = data['flour']
+        meat = data['meat']
+        sashimi = data['sashimi']
+        western= data['western']
+        age = data['age']
+        latitute = data['lat']
+        """
+        
+        # get current local time of server
+        now = time.localtime()
+        date = "%d:%d:%d" % (now.tm_hour, now.tm_min, now.tm_sec)
+        if DEBUG:
+            print date
+        
+        # get restaurant information from database
+        cur.execute("SELECT * FROM RestInfo")           # Send QUERY
+        
+        # Get result and put it into dictionary variable
+        restinfo = []
+        for row in cur.fetchall():
+            tmp = dict()
+            tmp['id'] = row[0]
+            tmp['name'] = row[1]
+            tmp['lat'] = row[2]
+            tmp['long'] = row[3]
+            tmp['rating'] = row[4]
 
-                restinfo.append(tmp)
-            if DEBUG:
-                print restinfo
-            
-            # usrinfo = data
-            # RuleBased.getRecommRest(usrinfo, date, restinfo)
-            self.wfile.write("Rule Based Algorithm")
-        elif pathinfo == 'machinelearning':
-            print ("Machine Learning Request")
-            # Do something
-            self.wfile.write("")
-            
-        else:
-            print ("Wrong Path...")
-            # Do something
-            # Notice user that the path is wrong
-            self.wfile.write("")
-
-
+            restinfo.append(tmp)
+        if DEBUG:
+            print restinfo
+        
+        # usrinfo = data
+        # RuleBased.getRecommRest(usrinfo, date, restinfo)
+        self.wfile.write("Rule Based Algorithm")
 
 if __name__ == '__main__':
 
